40fb09b61154debf1d204c82712e9278c22a9b04
vhsoto <vhsotoing@gmail.com>
2017-11-17 10:13:46 -0500

    Editar - eliminar habitaciones

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,15 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit, :update, :destroy]
+
   def index
     @rooms = Room.all
   end
 
   def new
     @room = Room.new
+  end
+
+  def edit    
   end
 
   def create
@@ -16,7 +21,24 @@ class RoomsController < ApplicationController
     end
   end
 
+  def update
+    if @room.update(room_params)
+      redirect_to rooms_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @room.destroy
+    redirect_to rooms_url
+  end
+
   protected
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
     end
